51c2e6b174777584ee84b498d12ee3bfaf284e1b
MadhavR97 <rathodmadhav215@gmail.com>
2025-04-18 15:06:15 +0530

    Mongodb_Connection_db

---
+++ Day 10/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const db = require('./config/db')
+const port = 3000
+
+const app = express()
+
+app.listen(port, (error)=>{
+    error ? console.log(error) : console.log(`Started server on port : ${port}`)
+})
