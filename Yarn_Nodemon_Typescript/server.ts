0bfa0f0d3152985d0d3ed8a09a483ff0ab976a88
MadhavR97 <rathodmadhav215@gmail.com>
2025-08-03 17:50:37 +0530

    Yarn_Nodemon_TypeScript

---
+++ Yarn_Nodemon_Typescript/server.ts
@@ -0,0 +1,16 @@
+// Install yarn nodemon
+// yarn add -D typescript ts-node @types/node nodemon
+
+// Create tsconfig.json
+// yarn tsc --init
+
+// For run nodemon in package.json add Script
+// {
+//     "scripts" : {
+//         "dev": "nodemon server.ts"
+//     }
+// }
+
+// Type "yarn dev" for run server.ts
+
+console.log('Hello Madhav')
